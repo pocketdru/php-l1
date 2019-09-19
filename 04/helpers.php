d946000895c87a2a4ee1cf26c26d08c2d0d0443c
pocketdru <pocketdru@gmail.com>
2019-09-19 15:18:14 -0600

    helpers.php fixed

--- 04/helpers.php
+++ 04/helpers.php
@@ -7,6 +7,11 @@ function println($string) {
 function separate($separator = '=', $repetitions = 12) {
     println(str_repeat($separator, $repetitions));
 }
+
+function print_full_name($first_name, $last_name) {
+    echo "Hi, my name is $first_name" . " " . "$last_name \n";
+}
+
 function test_name($name) {
     if (strlen($name) < 12) {
         echo "This is a short name!\n";
---
+++ 04/solution.php
@@ -0,0 +1,17 @@
+<?php 
+
+require "./helpers.php";
+
+$name_and_age = [
+    'Al Ali' => ['Mari', 20, "Colorado"],
+    'Vaider' => ['Darth', 52, "Death Star"]
+];
+
+foreach ($name_and_age as $person => $data) {
+
+    print_full_name($data[0], $person);
+    test_name($person, $data[0]);
+
+    test_sake($data[1]);
+    separate();
+}
